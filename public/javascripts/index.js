04ad402a3dcd43564cd075d8dae1d5e1c0b9bbbb
schulace <schulace2@gmail.com>
2017-10-24 10:33:09 -0400

    switched to using angular's

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,48 +12,25 @@ const pageModule = angular.module('postsPage', []);
  * bound to the variable's name, and the callbacks
  * handle updating values in the fields they run from.
 */
-pageModule.service('pgdata', function($http) {
+pageModule.service('pgdata', ['$http', function($http) {
     let $scope = this;
-    $scope.assignments = {
-        data:[],
-        observers:[]
-    };
-    $scope.classes = {
-        data:[],
-        observers:[]
-    };
-    $scope.addObserver = function(field, observer) {
-        const variable = $scope[field];
-        if(variable) {
-            variable.observers.push(observer);
-            observer(variable.data);
-        }
-    };
-    $scope.loggedIn = {
-        data:false,
-        observers:[]
-    };
+    $scope.assignments = [];
+    $scope.classes = [];
+    $scope.loggedIn = false;
     $scope.populate = function() {
         $http({
             url:'/api/assignments',
             method:'GET',
             responseType:'json'
         }).then(function(res) {
-            $scope.set('assignments', res.data);
-            $scope.set('loggedIn', true);
+            $scope.assignments = res.data;
+            $scope.loggedIn = true;
         }, function(err) {
-            $scope.set('loggedIn', false);
+            $scope.loggedIn = false;
             alert('not logged in');
         });
-    };
-    $scope.set = function(str, val) {
-        let v = $scope[str];
-        if(v) {
-            v.data = val;
-            v.observers.forEach(cb => cb(v.data));
-        }
-    };
-});
+    }; 
+}]);
 pageModule.controller('loginController', function($scope, $http, pgdata) {
     $scope.login = function () {
         $http({
@@ -68,20 +45,22 @@ pageModule.controller('loginController', function($scope, $http, pgdata) {
             function(err){
                 alert('login failed');
             });
-    };
+    }; 
     $scope.formdata = {
         email:'',
         password:''
     };
-    pgdata.addObserver('loggedIn', function(newval) {
+    $scope.$watch(() =>pgdata.loggedIn, function(newval, oldval) {
         $scope.loggedIn = newval;
     });
     pgdata.populate();
 });
 pageModule.controller('assignmentsController', function($scope, pgdata) {
-    pgdata.addObserver('assignments', function(newval) {
+    $scope.assignments = pgdata.assignments;
+    $scope.$watch(() => pgdata, function(newval) {
+        console.log('watch for assignments, ' + JSON.stringify(newval));
         $scope.assignments = newval;
-    });
+    }, true);
 });
 pageModule.controller('profileController', function($scipe, pgdata) {
     //TODO add create a thing to add / drop classes
